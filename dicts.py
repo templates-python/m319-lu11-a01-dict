e681ed3e14a808d153265be32062b4fc2b7585a2
Marcel Suter <marcel.suter@bzz.ch>
2025-12-18 09:07:47 +0100

    changed names for variables and arguments

--- dicts.py
+++ dicts.py
@@ -2,44 +2,44 @@ def main():
     """
     main program - creates some dictionaries and calls the functions
     """
-    cities = {'8000': 'Zürich', '1200': 'Genf', '1000': 'Lausanne', '3000': 'Bern', '8400': 'Winterthur',
+    cities1 = {'8000': 'Zürich', '1200': 'Genf', '1000': 'Lausanne', '3000': 'Bern', '8400': 'Winterthur',
               '6000': 'Luzern', '9000': 'St. Gallen', '6900': 'Lugano'}
-    add_cities(cities)
+    add_cities(cities1)
 
-    cities = {'8000': 'Zürich', '1200': 'Genf', '1000': 'Lausanne', '3000': 'Bern', '8400': 'Winterthur',
+    cities2 = {'8000': 'Zürich', '1200': 'Genf', '1000': 'Lausanne', '3000': 'Bern', '8400': 'Winterthur',
               '6000': 'Luzern', '9000': 'St. Gallen', '6900': 'Lugano'}
-    remove_cities(cities)
+    remove_cities(cities2)
 
-    cities = {'8000': 'Zürich', '1200': 'Genf', '1000': 'Lausanne', '3000': 'Bern', '8400': 'Winterthur',
+    cities3 = {'8000': 'Zürich', '1200': 'Genf', '1000': 'Lausanne', '3000': 'Bern', '8400': 'Winterthur',
               '6000': 'Luzern', '9000': 'St. Gallen', '6900': 'Lugano'}
-    find_cities(cities)
-    loop_cities(cities)
-    sort_cities(cities)
+    find_cities(cities3)
+    loop_cities(cities3)
+    sort_cities(cities3)
 
-def add_cities(cities):
+def add_cities(city_dict):
     """
     adds two cities
-    :param cities: dictionary of cities
+    :param city_dict: dictionary of cities
     :return: None
     """
     # TODO: add 2500 Biel
     # TODO: add 4000 Basel
-    print('add_cities:\n', cities)
+    print('add_cities:\n', city_dict)
 
-def remove_cities(cities):
+def remove_cities(dict_cities):
     """
     removes a city
-    :param cities: dictionary of cities
+    :param dict_cities: dictionary of cities
     :return: None
     """
     # TODO: remove the city with the zip-code 8400
-    print('remove_cities:\n', cities)
+    print('remove_cities:\n', dict_cities)
 
 
-def find_cities(cities):
+def find_cities(zip_city):
     """
     finds a city
-    :param cities: dictionary of cities
+    :param zip_city: dictionary of cities
     :return: None
     """
     print('find_city:')
@@ -48,20 +48,20 @@ def find_cities(cities):
     pass
 
 
-def loop_cities(cities):
+def loop_cities(city_dict):
     """
     prints all cities using a loop
-    :param cities: dictionary of cities
+    :param city_dict: dictionary of cities
     :return: None
     """
     print('loop_cities:')
     # TODO: print all cities in the list. output should be 'zip-code: name', i.e. '3000: Bern'
 
 
-def sort_cities(cities):
+def sort_cities(dict_cities):
     """
     sorts the cities by zip-code
-    :param cities: dictionary of cities
+    :param dict_cities: dictionary of cities
     :return: None
     """
     print('sort_cities:')
